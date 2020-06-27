15fe7acc3093925ad6c68b032d30b20715159998
Ramneek Singh <48586823+ramneek008@users.noreply.github.com>
2020-06-27 23:08:27 +0530

    statefull widget added

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget{
+  createState(){
+    return AppStateKeeper();
+  }
+}
+
+class AppStateKeeper extends State<App> {
+  
+  int myValue = 1;
 
   final barColor = const Color(0xFF26ae60);
   final bgColor = const Color(0xFFDAE0E2);
 
   Widget build(context){
     return MaterialApp(
-    home: Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor:barColor,
-        child: Icon(Icons.add, color: bgColor),
-        onPressed: (){}
+      home: Scaffold(
+        body: Text('You pressed: $myValue'),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor:barColor,
+          child: Icon(Icons.add, color: bgColor),
+          onPressed: (){
+            setState((){
+              myValue = myValue + 1;
+            });
+          }
+        ),
+        backgroundColor: bgColor,
+        appBar: AppBar(
+          title: Text('App'),
+          backgroundColor: barColor,
+        ),
       ),
-      backgroundColor: bgColor,
-      appBar: AppBar(
-        title: Text('App'),
-        backgroundColor: barColor,
-      ),
-    ),
-  );
+    );
   }
 }
