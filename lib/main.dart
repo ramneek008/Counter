76856d52d16f5ee6a91a2ae49ce9a4d2a8bda6b5
Ramneek Singh <48586823+ramneek008@users.noreply.github.com>
2020-06-27 17:17:56 +0530

    scaffolding

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+//TODO: import library
+import 'package:flutter/material.dart';
+
+
+//TODO: create a main function
+void main(){
+  //TODO: Create a widget
+
+  final barColor = const Color(0xFF26ae60);
+  final bgColor = const Color(0xFFDAE0E2);
+
+  var app = MaterialApp(
+    home: Scaffold(
+      backgroundColor: bgColor,
+      appBar: AppBar(
+        backgroundColor: barColor,
+      ),
+    ),
+  );
+
+  //TODO: Run that widget on mobile device
+  runApp(app);
+}
+
